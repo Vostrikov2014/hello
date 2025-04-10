fa11a2735001bb99171e29d1454aef5c67e16d77
45VostrikovDA <45vostrikovda@region.cbr.ru>
2025-04-10 16:30:47 +0300

    2

--- src/main/java/com/example/hello/config/SecurityConfig.java
+++ src/main/java/com/example/hello/config/SecurityConfig.java
@@ -3,30 +3,38 @@ package com.example.hello.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Value("${keySetURI}")
-    private String keySetUri;
+    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+    String jwkSetUri;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http.oauth2ResourceServer(
-                c -> c.jwt(
-                        j -> j.jwkSetUri(keySetUri)
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers(HttpMethod.GET, "/message/**").hasAuthority("SCOPE_message:read")
+                        .requestMatchers(HttpMethod.POST, "/message/**").hasAuthority("SCOPE_message:write")
+                        .anyRequest().authenticated()
                 )
-        );
-
-        http.authorizeHttpRequests(
-                c -> c.anyRequest().authenticated()
-        );
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(withDefaults()));
 
         return http.build();
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withJwkSetUri(this.jwkSetUri).build();
     }
 }
--- src/main/java/com/example/hello/controller/DemoController.java
+++ src/main/java/com/example/hello/controller/DemoController.java
@@ -1,6 +1,11 @@
 package com.example.hello.controller;
 
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
